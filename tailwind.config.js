d310cef1c8de528c1083afc205b178a62e7d8dd5
victor <jvslnirvana@gmail.com>
2024-12-08 21:06:54 -0300

    gitignore add

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js,css}"],
+  content: ["./src/**/*.{html,js}"], // Adicione as extensões usadas no seu projeto
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
